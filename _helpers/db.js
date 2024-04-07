1df292011c52f963643ee277b51cae969a90e5a0
unknown <johnaxelguidayawan3@gmail.com>
2024-04-07 23:56:02 +0800

    second commit

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,28 +1,27 @@
 const config = require('config.json');
 const mysql = require('mysql2/promise');
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 
 module.exports = db = {};
 
-initialize(); 
+initialize();
 
-async function initialize(){
-    // create db if it doesn't already exist 
-    const { host, port, user, password, database } = config.databasel;
-    const connection = await mysql.createConnection({ host, port, user, password });
-    await connection. query(`CREATE DATABASE IF NOT EXIST \` ${database}\`;`);
 
-    //connect to db 
-    const sequelize = new Sequelize (database, user, password, { dialect: 'mysql'});
+async function initialize() {
 
-    //init models and add them to the exported db object 
+    const { host, port, user, password, database} = config.database;
+    const connection = await mysql.createConnection({host, port, user, password});
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql'});
+
     db.Account = require('../accounts/account.model')(sequelize);
     db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
 
-    // define relationships
-    db.Account.hasMan(db.RefreshToken, {onDelete: 'CASCADE'});
+
+    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE'});
     db.RefreshToken.belongsTo(db.Account);
 
-    //sync all models with database
+
     await sequelize.sync();
 }
